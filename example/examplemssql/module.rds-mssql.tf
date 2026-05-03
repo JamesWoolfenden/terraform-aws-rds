524c5889c32c4d56f296afc1138d2441e724dd52
James Woolfenden <james.woolfenden@gmail.com>
2026-05-04 00:32:39 +0100

    chore: pin dependencies to immutable SHAs via ghat

--- example/examplemssql/module.rds-mssql.tf
+++ example/examplemssql/module.rds-mssql.tf
@@ -12,5 +12,5 @@ module "rds-mssql" {
   vpc_id            = data.aws_vpc.examplea[0].id
 }
 module "data" {
-  source = "git::https://github.com/JamesWoolfenden/terraform-http-ip.git?ref=5769331633debca683a81a38470083a0abd39049"
+  source = "git::https://github.com/JamesWoolfenden/terraform-http-ip.git?ref=2f3cef24e667fb840a3d3481f5a1aaa5a1ac7d28" #v0.3.14
 }
